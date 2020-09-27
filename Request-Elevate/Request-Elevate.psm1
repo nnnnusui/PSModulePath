e64e4ef173e08c079b239520777fde718419234c
nnnnusui <nnnnusui@gmail.com>
2020-09-28 03:33:12 +0900

    :new: Request-Elevate

---
+++ Request-Elevate/Request-Elevate.psm1
@@ -0,0 +1,15 @@
+﻿function Request-Elevate {
+param(
+    $callStackBackDepth = 0
+    )
+    # 管理者権限が無ければ要求
+    If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {   
+        # $arguments = "& '" + $myinvocation.mycommand.definition + "'"
+        # 自身を再起動
+        $command = Get-ScriptCommand($callStackBackDepth + 1)
+        Start-Process powershell -Verb runAs -ArgumentList $command
+        $true
+    } else {
+        $false
+    }
+}
