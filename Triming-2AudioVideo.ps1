7b81fd0adf6e6adf78e3422733f6c4b284ca1721
nnnnusui <nnnnusui@gmail.com>
2020-10-11 18:16:59 +0900

    :new: Video triming shortcut
    for Geforce-Experience's multi audio track clip.mp4

---
+++ Triming-2AudioVideo.ps1
@@ -0,0 +1,50 @@
+﻿$outputRoot = Convert-Path "~backup"
+$pwd = (Convert-Path .)
+
+$path = $Args[0]
+if ($path -eq $null) {
+    $path = Read-Host "どの動画を"
+}
+
+$originalPathStr = (Convert-Path $path)
+$original = Get-Item $originalPathStr
+if (!$originalPathStr.Contains($pwd)) {
+    Write-Error "ターゲット('$original')は '$pwd' より下の階層に存在しなければいけません"
+    pause; throw
+}
+$relative = Resolve-Path -Relative $original
+$relativeParent = Split-Path $relative -Parent
+if ($relativeParent -eq ".") {
+    Write-Error "ターゲット('$original')は '$pwd' より1層以上深いパスに存在しなければいけません"
+    pause; throw
+}
+$baseName = $original.BaseName
+$extension = $original.Extension
+$outputParent = "$outputRoot\$relativeParent"
+if (!(Test-Path $outputParent)) {
+    New-Item $outputParent -ItemType Directory
+}
+$outputPath = "$outputParent\$baseName$extension"
+
+echo "from: $original"
+echo "to  : $outputPath"
+$duration = Read-Host "いつから(hh:mm:ss)"
+
+ffmpeg `
+    -ss $duration `
+    -i $original `
+    -vcodec copy `
+    -acodec copy `
+    -map 0 `
+    $outputPath
+
+if ($?) {
+    $parent = Split-Path $original -Parent
+    $moveTo = "$parent\trimed\"
+    if (!(Test-Path $moveTo)) {
+        New-Item $moveTo -ItemType Directory
+    }
+    Move-Item $original "$parent\trimed\"
+} else {
+    pause; throw
+}
